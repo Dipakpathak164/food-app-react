b40bc2f0f6657290d0eb918ba1b38f399912ae1a
Dipak Pathak <dipak@techasoft.com>
2025-04-08 14:28:00 +0530

    add to cart

--- food-app-frontend/src/components/CartIcon.jsx
+++ food-app-frontend/src/components/CartIcon.jsx
@@ -1,17 +1,26 @@
 // components/CartIcon.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import { useCart } from '../context/CartContext';
+import CartDrawer from './CartDrawer';
 
 const CartIcon = ({ imagePath }) => {
   const { cart } = useCart();
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <span className="cart_count mx-3 position-relative">
-      <span className="count_value position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-        {cart?.length || 0}
+    <>
+      <span
+        className="cart_count mx-3 position-relative"
+        role="button"
+        onClick={() => setIsOpen(true)}
+      >
+        <span className="count_value position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+          {cart?.length || 0}
+        </span>
+        <img src={`${imagePath}bag.png`} alt="Cart" />
       </span>
-      <img src={`${imagePath}bag.png`} alt="Cart" />
-    </span>
+      <CartDrawer isOpen={isOpen} onClose={() => setIsOpen(false)} />
+    </>
   );
 };
 
